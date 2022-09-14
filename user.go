10b4ae36e1eac4c12b4afec0548ad759f4634722
Pieter-Jan Briers <pieterjan.briers@level27.be>
2022-09-14 10:51:13 +0200

    API functions for org user SSH keys.

--- user.go
+++ user.go
@@ -14,6 +14,47 @@ func (c Client) OrganisationUserGetSingle(organisationID int, userID int) (User,
 	return resp.Data, err
 }
 
+// GET /organisations/{organisationID}/users/{userID}/sshkeys
+func (c Client) OrganisationUserGetSshKeys(organisationID int, userID int, get CommonGetParams) ([]SshKey, error) {
+	var resp struct {
+		Data []SshKey `json:"sshkeys"`
+	}
+
+	endpoint := fmt.Sprintf("organisations/%d/users/%d/sshkeys?%s", organisationID, userID, formatCommonGetParams(get))
+	err := c.invokeAPI("GET", endpoint, nil, &resp)
+
+	return resp.Data, err
+}
+
+// Find all SSH keys on a user matching the specified description.
+func (c *Client) OrganisationUserSshKeysLookup(organisationID int, userID int, name string) ([]SshKey, error) {
+	results := []SshKey{}
+	systems, err := c.OrganisationUserGetSshKeys(organisationID, userID, CommonGetParams{Filter: name})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, system := range systems {
+		if system.Description == name {
+			results = append(results, system)
+		}
+	}
+
+	return results, err
+}
+
+// GET /organisations/{organisationID}/users/{userID}/sshkeys/{sshKeyID}
+func (c *Client) OrganisationUserSshKeysGetSingle(organisationID int, userID int, sshKeyID int) (SshKey, error) {
+	var resp struct {
+		Data SshKey `json:"sshkey"`
+	}
+
+	endpoint := fmt.Sprintf("organisations/%d/users/%d/sshkeys/%d", organisationID, userID, sshKeyID)
+	err := c.invokeAPI("GET", endpoint, nil, &resp)
+
+	return resp.Data, err
+}
+
 type User struct {
 	ID             int      `json:"id"`
 	Username       string   `json:"username"`
